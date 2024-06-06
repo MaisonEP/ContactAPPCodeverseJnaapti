dc9894d027147a5644b20ab19cce9246f31705a7
MaisonEP <erl.maison@hotmail.co.uk>
2024-06-06 10:40:35 +0100

    Island that will contain contacts

---
+++ src/app/AppIsland/Island.js
@@ -0,0 +1,10 @@
+import "./Island.css";
+import React from "react";
+
+export function Ocean() {
+	return (
+		<div className="Ocean">
+			<div className="island"></div>
+		</div>
+	);
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
+import { Ocean } from "./AppIsland/Island";
 
 export default function Home() {
-	return <div>Helo</div>;
+	return <Ocean></Ocean>;
 }
